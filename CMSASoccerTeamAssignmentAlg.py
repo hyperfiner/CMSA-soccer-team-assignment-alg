5f6c99273e03622458a358cebf5050353fdbe2de
Rob Fine <37419405+hyperfiner@users.noreply.github.com>
2025-09-18 14:40:34 -0500

    Updated to include assignment based on ranking as alternative to ranking based on experience in competitive division. Also improved commenting and print statements to self-document decisions made by the algorithm as it runs.

--- CMSASoccerTeamAssignmentAlg.py
+++ CMSASoccerTeamAssignmentAlg.py
@@ -6,6 +6,7 @@ import os
 import json
 
 def delete_unneeded_columns(data_frame):
+# Not needed for 2025 iteration
 
   # Delete one or more columns by name
   columns_to_keep = ["First Name","Last Name","Have you ever played soccer before?","Please rate your soccer skill level.","Have you played in this league's competitive division in the past three years?","Is there another player with whom you would like to be on the same team? (Requests will be considered but cannot be guaranteed.)","What position do you prefer to play?","What other position would you play, if needed?"] 
@@ -17,9 +18,10 @@ def rename_columns(data_frame):
 
   # Create a dictionary with your transformation rules
   transformation_dict = {
-    "Have you ever played soccer before?": "Experience_YesNo",
+    #"Have you ever played soccer before?": "Experience_YesNo",
     "Please rate your soccer skill level.": "Skill_1to5",
-    "Have you played in this league's competitive division in the past three years?": "Competitive_YesNo",
+    "Captain Ranking": "Rank",
+    "Have you played in this league's competitive division during the recent seasons?": "Competitive_YesNo",
     "Is there another player with whom you would like to be on the same team? (Requests will be considered but cannot be guaranteed.)": "Friend",
     "What position do you prefer to play?": "Position1",
     "What other position would you play, if needed?": "Position2" 
@@ -63,7 +65,7 @@ def add_identifier(data_frame):
 
   return data_frame
 
-def make_team_assignments(data_frame, n_teams, team_colors):
+def make_team_assignments(data_frame, n_teams, team_colors, sort_by_rank=True):
 
   data_frame["Assigned"] = False
   data_frame["Do Not Reassign"] = False
@@ -75,34 +77,70 @@ def make_team_assignments(data_frame, n_teams, team_colors):
   ## Goalkeeper is a special case
   ###############################
 
-  # Grab subset of registrants that have selected Goalkeeper as a first- or second-choice position
-  temp_data_frame = data_frame[(data_frame["Position1"] == "Goalkeeper") | (data_frame["Position2"] == "Goalkeeper")]
-  # Randomly re-order subset of registrants
-  temp_shuffled_data_frame = temp_data_frame.sample(frac=1, random_state=42).reset_index(drop=True)
+  ## Randomly assign players that have selected Goalkeeper
+  ## as their first- or second-choice position
 
-  # Distribute registrants into teams in a round-robin fashion
-  for idx,row in temp_shuffled_data_frame.iterrows():
-    
-    # Pull out the unique identifier for this registrant
-    unique_ID = temp_shuffled_data_frame.loc[idx,"Unique_ID"]
-    # Assign the registrant to the next team
-    data_frame.loc[unique_ID,"Team"] = team_colors[last_assigned_team%n_teams]
-    data_frame.loc[unique_ID,"Assigned"] = True
-    data_frame.loc[unique_ID,"Do Not Reassign"] = True # Goalkeepers should not be swappable later on
-    print("I'm assigning {0} {1} to {2}".format(data_frame.loc[unique_ID,"First Name"],data_frame.loc[unique_ID,"Last Name"],team_colors[last_assigned_team%n_teams]))
-    last_assigned_team += 1
+  print("Now randomly assigning Goalkeepers to teams")
+  print("-------------------------------------")
+  print("-------------------------------------")
+  print("-------------------------------------\n")
+
+  for i_position in ["Position1","Position2"]:
+
+    # Grab subset of registrants
+    temp_data_frame = data_frame[(data_frame[i_position] == "Goalkeeper")]
+    # Randomly re-order subset of registrants
+    temp_shuffled_data_frame = temp_data_frame.sample(frac=1, random_state=42).reset_index(drop=True)
+
+    # Distribute registrants into teams in a round-robin fashion
+    for idx,row in temp_shuffled_data_frame.iterrows():
+      
+      # Pull out the unique identifier for this registrant
+      unique_ID = temp_shuffled_data_frame.loc[idx,"Unique_ID"]
+
+      # If the player already has a team assignment skip (this should only be true for 
+      # anyone that list GK as their primary and secondary position)
+      if data_frame.loc[unique_ID,"Assigned"] == True:
+        continue
+
+      # Assign the registrant to the next team
+      data_frame.loc[unique_ID,"Team"] = team_colors[last_assigned_team%n_teams]
+      data_frame.loc[unique_ID,"Assigned"] = True
+      data_frame.loc[unique_ID,"Do Not Reassign"] = True # Goalkeepers should not be swappable later on
+      print("I'm assigning {0} {1} to {2}".format(data_frame.loc[unique_ID,"First Name"],data_frame.loc[unique_ID,"Last Name"],team_colors[last_assigned_team%n_teams]))
+      last_assigned_team += 1
 
   ## Randomly assign players separately in 
   ## categories of position and competitiveness  
   #############################################
 
+  print("\nNow randomly assigning all other positions to teams")
+  print("-------------------------------------")
+  print("-------------------------------------")
+  print("-------------------------------------")
+
+  if sort_by_rank:
+    print_statement = "I'm going to sort based on player's captains-identified ranking.\n"
+    sort_categories = [-1,1,2,3,4,5]
+    sort_key = "Rank"
+  else: # sort by 
+    print_statement = "I'm going to sort based on player's history with competitive division.\n"
+    sort_categories = ["Yes","No"]
+    sort_key = "Competitive_YesNo"
+
+  print(print_statement)
+
   # Loop over positions
   for position in ["Forward","Midfielder","Defender"]:
-    # Loop over competitiveness
-    for comp in ["Yes","No"]:
-      
-      # Grab subset of registrants that have selected position and are (not) competitive
-      temp_data_frame = data_frame[(data_frame["Position1"] == position) & (data_frame["Competitive_YesNo"] == comp)]
+    print("\nNow assigning players with preferred position: {0}".format(position))
+    print("-------------------------------------")
+    print("-------------------------------------")
+    # Loop over specified categories
+    for sort_it in sort_categories:
+      print("\nNow assigning players with {0}: {1}".format(sort_key,sort_it))
+      print("-------------------------------------")
+      # Grab subset of registrants that have selected position and satisfy sort criterion 
+      temp_data_frame = data_frame[(data_frame["Position1"] == position) & (data_frame[sort_key] == sort_it)]
       # Randomly re-order subset of registrants
       temp_shuffled_data_frame = temp_data_frame.sample(frac=1, random_state=42).reset_index(drop=True)
 
@@ -123,13 +161,20 @@ def make_team_assignments(data_frame, n_teams, team_colors):
 
   return data_frame
 
-def reassign_player_pairs(data_frame,player_pairs):
+def reassign_player_pairs(data_frame, player_pairs, sort_by_rank=True):
+
+  print("\n\nNow reassigning players to satisfy player pair requests as best as able.")
+  print("-------------------------------------")
+  print("-------------------------------------")
+  print("-------------------------------------\n")
 
   for pair in player_pairs:
     player_stay = pair[1].split()
     frame_player_stay = data_frame[(data_frame["First Name"] == player_stay[0]) & (data_frame["Last Name"] == " ".join(player_stay[1:]))]
     player_swap = pair[0].split()
     frame_player_swap = data_frame[(data_frame["First Name"] == player_swap[0]) & (data_frame["Last Name"] == " ".join(player_swap[1:]))]
+
+    print("Now attempting to place {0} {1} and {2} {3} on the same team.".format(player_stay[0],player_stay[1],player_swap[0],player_swap[1]))
 
     # Ensure there's a valid row by checking if the filtered DataFrame is not empty
     if frame_player_stay.empty or frame_player_swap.empty:
@@ -149,6 +194,7 @@ def reassign_player_pairs(data_frame,player_pairs):
     # Are these players already on the same team?
     if team_stay == team_swap:
       print("{0} {1} and {2} {3} are already on the same team!".format(player_stay[0],player_stay[1],player_swap[0],player_swap[1]))
+      print("-------------------------------------\n")
 
       ## Both players of the pair shouldn't get moved in future swaps
       data_frame.loc[unique_ID_stay,"Do Not Reassign"] = True
@@ -159,11 +205,25 @@ def reassign_player_pairs(data_frame,player_pairs):
 
     position_swap = frame_player_swap.loc[0,"Position1"]
     comp_swap = frame_player_swap.loc[0,"Competitive_YesNo"]
+    rank_swap = frame_player_swap.loc[0,"Rank"]
 
-    potential_players_to_swap = data_frame[(data_frame["Team"] == team_stay) & (data_frame["Position1"] == position_swap) & (data_frame["Competitive_YesNo"] == comp_swap) & (data_frame["Do Not Reassign"] != True)]
+    if sort_by_rank:
+      potential_players_to_swap = data_frame[(data_frame["Team"] == team_stay) & (data_frame["Position1"] == position_swap) & (data_frame["Rank"] == rank_swap) & (data_frame["Do Not Reassign"] != True)]
+    else:
+      potential_players_to_swap = data_frame[(data_frame["Team"] == team_stay) & (data_frame["Position1"] == position_swap) & (data_frame["Competitive_YesNo"] == comp_swap) & (data_frame["Do Not Reassign"] != True)]
 
+    ## If no matches try again, but allow for secondary position of player to be used for matching
+    if potential_players_to_swap.empty:
+      print("No suitable swaps identified using primary criteria; expanding to allow secondary position to be used for swap")
+      if sort_by_rank:
+        potential_players_to_swap = data_frame[(data_frame["Team"] == team_stay) & (data_frame["Position2"] == position_swap) & (data_frame["Rank"] == rank_swap) & (data_frame["Do Not Reassign"] != True)]
+      else:
+        potential_players_to_swap = data_frame[(data_frame["Team"] == team_stay) & (data_frame["Position2"] == position_swap) & (data_frame["Competitive_YesNo"] == comp_swap) & (data_frame["Do Not Reassign"] != True)]
+
+    ## If still no matches, give up on swap
     if potential_players_to_swap.empty:
       print("I can't execute the swap you're attempting because there are no equivalent folks to switch")
+      print("-------------------------------------\n")
       continue
   
     random_player_to_swap = potential_players_to_swap.sample(n=1)
@@ -173,6 +233,7 @@ def reassign_player_pairs(data_frame,player_pairs):
     random_player_to_swap_unique_ID = random_player_to_swap.loc[0,"Unique_ID"]
  
     print("I'm going to swap {0} {1} and {2} {3}".format(player_swap[0], player_swap[1], random_player_to_swap_FirstName,random_player_to_swap_LastName))
+    print("-------------------------------------\n")
 
     ## Perform swap
     data_frame.loc[random_player_to_swap_unique_ID,"Team"] = team_swap
@@ -195,7 +256,12 @@ def sort_data_by_team(data_frame):
  
 def print_stats(data_frame,team_colors,summary_file_path):
 
-  columns_to_skip = ["First Name","Last Name","Friend","Unique_ID","Do Not Reassign"]
+  print("\n\nNow printing summary table.")
+  print("-------------------------------------")
+  print("-------------------------------------")
+  print("-------------------------------------\n")
+
+  columns_to_skip = ["First Name","Last Name","Friend","Unique_ID","Assigned","Do Not Reassign","Skill_1to5"]
   summary_table = []
 
   # Loop through each column except "Team" to get the counts per team
@@ -215,7 +281,7 @@ def print_stats(data_frame,team_colors,summary_file_path):
   summary_table.to_csv(summary_file_path, sep='\t')
 
   # Define the manual line to prepend
-  manual_line = "\tExperience_YesNo\t\t\tSkill_1to5\t\t\t\t\tCompetitive_YesNo\t\tPosition1\t\t\t\tPosition2\t\t\t\t\n"
+  manual_line = "\tRank\t\t\t\t\t\tPosition1\t\t\t\tPosition2\t\t\t\tCompetitive_YesNo\t\t\n"
   
   # Prepend the manual line to the summary table content
   temp_content = ""
@@ -228,9 +294,9 @@ def print_stats(data_frame,team_colors,summary_file_path):
 ######### I/O #########
 #######################
  
-file_path = "input/2024_Fall/Fall24_Roster_Open.csv"
-output_file_path = "output/test/Fall24_Roster_Open_randomized.csv"
-summary_file_path = "output/test/Fall24_Roster_Open_summary_table.txt"
+file_path = "input/2025_Fall/Fall25_Roster_Open.csv"
+output_file_path = "output/2025_Fall/Fall25_Roster_Open_randomized.csv"
+summary_file_path = "output/2025_Fall/Fall25_Roster_Open_summary_table.txt"
 
 #### Process Data ####
 ######################
@@ -238,7 +304,7 @@ summary_file_path = "output/test/Fall24_Roster_Open_summary_table.txt"
 # Read the CSV into a DataFrame
 data_frame = pd.read_csv(file_path)
 # Delete unneeded columns
-data_frame = delete_unneeded_columns(data_frame)
+#data_frame = delete_unneeded_columns(data_frame)
 # Rename column headers
 data_frame = rename_columns(data_frame)
 # Transform column contents
@@ -251,7 +317,7 @@ data_frame = add_identifier(data_frame)
 
 n_teams = 6
 team_colors = ["Fandango","Mikado","Gamboge","Amaranth","Glaucous","Vermilion"]
-with open("input/2024_Fall/Fall24_Open_player_pairs.json", "r", encoding="utf-8") as f:
+with open("input/2025_Fall/Fall25_Open_player_pairs.json", "r", encoding="utf-8") as f:
     player_pairs = json.load(f)
 
 # Make pseudo-random team assignments
